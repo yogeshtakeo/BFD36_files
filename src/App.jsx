399431059ffe40f047b47fe1a58ab28748ded85b
karki37 <prakashkarki37@outlook.com>
2023-09-04 11:21:23 -0400

    html semantics

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div class="container">
+        <h5>developer</h5>
+        <header>
+          <h1>Frontend Developer</h1>
+        </header>
+        <div>
+          <nav class="bar">
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li>
+                <a href="#">Terms of use</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <main class="student">
+          <section>
+            <p>
+              I am a new student learning front end react development at Takeo
+              Bootcamp. It's been a week of the react bootcamp where I explore
+              doctype html, html elements, tags and semantics. Along with that I
+              learned git- a version control system that track and manage source
+              code enabling multiple developers to work together.
+            </p>
+            <p>
+              Learning takes time and patience. You have to face frustation and
+              different struggle scenarios during the learning journey. But I
+              beleive over the period of time, you can acheive what you want.
+            </p>
+          </section>
+        </main>
+
+        <footer>
+          <p>Copyright © 2023. All right reserved.</p>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
